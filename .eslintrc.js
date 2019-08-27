f3cebcc61901768fc1463b33f6462232f536960c
hextion <100ishundred@gmail.com>
2019-08-27 20:44:58 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {}
+};
