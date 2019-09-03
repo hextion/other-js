afcb09fe3b0d7428a537af372b7594d256813287
hextion <100ishundred@gmail.com>
2019-09-03 12:48:20 +0300

    Added equal sides of an array

---
+++ src/equal-sides-of-an-array.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @param {[number]} numbers
+ */
+function equalsAt(numbers) {
+  const leftToRight = numbers.reduce((acc, num) => {
+    const previous = acc[acc.length - 1] || 0;
+    return acc.concat(num + previous);
+  }, []);
+  const rightToLeft = numbers.reduceRight((acc, num) => {
+    const [previous = 0] = acc;
+    return [num + previous].concat(acc);
+  }, []);
+  for (let i = 0; i < numbers.length; i++) {
+    if (leftToRight[i] === rightToLeft[i]) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+module.exports = { equalsAt };
---
+++ src/equal-sides-of-an-array.spec.js
@@ -0,0 +1,8 @@
+const { equalsAt } = require('./equal-sides-of-an-array');
+
+it('works', () => {
+  expect(equalsAt([1, 2, 3, 4, 3, 2, 1])).toEqual(3);
+  expect(equalsAt([1, 5, 4, 3, 2, 1])).toEqual(2);
+  expect(equalsAt([6, 4, 3, 2, 1])).toEqual(1);
+  expect(equalsAt([1, 4, 3, 2, 1])).toEqual(0);
+});
