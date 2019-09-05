7e94cf2dc4e7f2f82b1ef2ab834dd9685bcdaff4
hextion <100ishundred@gmail.com>
2019-09-05 18:56:51 +0300

    Updated merge-sort test

--- src/merge-sort.spec.js
+++ src/merge-sort.spec.js
@@ -8,4 +8,21 @@ it('sorts array', () => {
   expect(mergeSort([2, 1])).toEqual([1, 2]);
   expect(mergeSort([-1, 1])).toEqual([-1, 1]);
   expect(mergeSort([-1, -1, -1])).toEqual([-1, -1, -1]);
+  expect(mergeSort([10, 1, 3, 2, 5, 7, 9, 10, 11, 123, 213, 3213, 3213, 3213, 10000])).toEqual([
+    1,
+    2,
+    3,
+    5,
+    7,
+    9,
+    10,
+    10,
+    11,
+    123,
+    213,
+    3213,
+    3213,
+    3213,
+    10000,
+  ]);
 });
