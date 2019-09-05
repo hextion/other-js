5b3c5ab0c6bbca93edf4484b580061d223dfd32e
hextion <100ishundred@gmail.com>
2019-09-05 18:53:57 +0300

    Added recursive merge to merge-sort

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -3,6 +3,7 @@
  * @param {[number]} arr1
  * @param {[number]} arr2
  */
+// eslint-disable-next-line no-unused-vars
 function merge(arr1, arr2) {
   let res = [];
   let i = 0;
@@ -24,6 +25,24 @@ function merge(arr1, arr2) {
   }
   return res;
 }
+/**
+ *
+ * @param {[number]} arr1
+ * @param {[number]} arr2
+ */
+function mergeRecursive(arr1, arr2) {
+  if (arr1.length === 0) {
+    return arr2;
+  }
+  if (arr2.length === 0) {
+    return arr1;
+  }
+  const el1 = arr1[0];
+  const el2 = arr2[0];
+  return el1 < el2
+    ? [el1].concat(mergeRecursive(arr1.slice(1), arr2))
+    : [el2].concat(mergeRecursive(arr1, arr2.slice(1)));
+}
 
 /**
  *
@@ -34,7 +53,7 @@ function mergeSort(arr) {
     return arr;
   }
   const middle = Math.floor(arr.length / 2);
-  return merge(mergeSort(arr.slice(0, middle)), mergeSort(arr.slice(middle)));
+  return mergeRecursive(mergeSort(arr.slice(0, middle)), mergeSort(arr.slice(middle)));
 }
 
 module.exports = { mergeSort };
