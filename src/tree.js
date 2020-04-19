532bb5d3b24efc3ec86dc12adbeb0fc94f92aba5
hextion <100ishundred@gmail.com>
2020-04-20 00:31:39 +0300

    Improved makeTree

--- src/tree.js
+++ src/tree.js
@@ -1,16 +1,31 @@
+const defaultOptions = {
+  keySelector: item => item.key,
+  parentKeySelector: item => item.parentKey,
+  transform: item => item,
+};
+
 /**
  * @param {Array<object>} arr array
+ * @param {object} [options] options
  * @returns {Array<object>} tree
  */
-function makeTree(arr) {
-  const shalow = arr.map(each => ({ ...each }));
+function makeTree(arr, options = defaultOptions) {
+  const { keySelector, parentKeySelector, transform } =
+    options === defaultOptions
+      ? options
+      : {
+          ...defaultOptions,
+          ...(options || {}),
+        };
+  const shalow = arr.map(each => transform({ ...each }));
   return shalow
-    .map(each => {
-      let children = shalow.filter(({ parentId }) => parentId === each.id);
+    .map(item => {
+      const key = keySelector(item);
+      let children = shalow.filter(item => parentKeySelector(item) === key);
       children = children.length > 0 ? children : null;
-      return Object.assign(each, { children });
+      return Object.assign(item, { children });
     })
-    .filter(({ parentId }) => parentId === null);
+    .filter(item => parentKeySelector(item) === null);
 }
 
 module.exports = { makeTree };
--- src/tree.spec.js
+++ src/tree.spec.js
@@ -4,38 +4,121 @@ it('should return empty array', () => {
   expect(makeTree([])).toMatchObject([]);
 });
 
-it('should return tree', () => {
-  expect(
-    makeTree([
+describe('should return tree', () => {
+  it('should use default options', () => {
+    expect(
+      makeTree([
+        {
+          key: 1,
+          parentKey: null,
+        },
+        {
+          key: 2,
+          parentKey: 1,
+        },
+        {
+          key: 3,
+          parentKey: null,
+        },
+      ])
+    ).toMatchObject([
+      {
+        key: 1,
+        parentKey: null,
+        children: [
+          {
+            key: 2,
+            parentKey: 1,
+            children: null,
+          },
+        ],
+      },
+      {
+        key: 3,
+        parentKey: null,
+        children: null,
+      },
+    ]);
+  });
+
+  it('should use passed options', () => {
+    expect(
+      makeTree(
+        [
+          {
+            id: 1,
+            parentId: null,
+          },
+          {
+            id: 2,
+            parentId: 1,
+          },
+          {
+            id: 3,
+            parentId: null,
+          },
+        ],
+        {
+          keySelector: item => item.id,
+          parentKeySelector: item => item.parentId,
+        }
+      )
+    ).toMatchObject([
       {
         id: 1,
         parentId: null,
-      },
-      {
-        id: 2,
-        parentId: 1,
+        children: [
+          {
+            id: 2,
+            parentId: 1,
+            children: null,
+          },
+        ],
       },
       {
         id: 3,
         parentId: null,
+        children: null,
       },
-    ])
-  ).toMatchObject([
-    {
-      id: 1,
-      parentId: null,
-      children: [
+    ]);
+
+    expect(
+      makeTree(
+        [
+          {
+            id: 1,
+            parentId: null,
+          },
+          {
+            id: 2,
+            parentId: 1,
+          },
+          {
+            id: 3,
+            parentId: null,
+          },
+        ],
         {
-          id: 2,
-          parentId: 1,
-          children: null,
-        },
-      ],
-    },
-    {
-      id: 3,
-      parentId: null,
-      children: null,
-    },
-  ]);
+          transform: item => ({ key: item.id, parentKey: item.parentId }),
+        }
+      )
+    ).toMatchObject([
+      {
+        key: 1,
+        parentKey: null,
+        children: [
+          {
+            key: 2,
+            parentKey: 1,
+            children: null,
+          },
+        ],
+      },
+      {
+        key: 3,
+        parentKey: null,
+        children: null,
+      },
+    ]);
+  });
 });
