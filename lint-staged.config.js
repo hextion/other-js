56bd1ad8fa8ed11a32b5814bdec1021b7719fbca
hextion <100ishundred@gmail.com>
2020-04-22 16:31:20 +0300

    Fixed git hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,15 @@
+const prettier = 'prettier --list-different';
+const eslint = 'eslint --max-warnings 0';
+
+/**
+ * Return array of passed commands
+ *
+ * @param  {...string} commands commands to run
+ * @returns {string[]} commands
+ */
+const run = (...commands) => commands;
+
 module.exports = {
-  '*.{json,md,yml}': ['prettier --write', 'git add'],
-  '*.js': ['eslint --fix', 'git add'],
+  '*.{json,md,yml}': run(prettier),
+  '*.js': run(prettier, eslint),
 };
