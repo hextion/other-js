586939c84be685524e53f9681612bd98a94e1ebb
hextion <100ishundred@gmail.com>
2021-09-29 18:48:24 +0300

    Added sameStructureAs

---
+++ src/same-structure-as.js
@@ -0,0 +1,21 @@
+/**
+ * Check if arrays has the same structures
+ *
+ * @param {Array} a array
+ * @param {*} b other array
+ * @returns {boolean} if arrays has the same structures
+ */
+function sameStructureAs(a, b) {
+  return (
+    a.length === b.length &&
+    a.every((item, idx) => {
+      const anotherItem = b[idx];
+      return (
+        (Array.isArray(item) && Array.isArray(anotherItem) && sameStructureAs(item, anotherItem)) ||
+        (!Array.isArray(item) && !Array.isArray(anotherItem))
+      );
+    })
+  );
+}
+
+module.exports = { sameStructureAs };
---
+++ src/same-structure-as.spec.js
@@ -0,0 +1,23 @@
+const { sameStructureAs } = require('./same-structure-as');
+
+it('returns true', () => {
+  expect(sameStructureAs([1, 1, 1], [2, 2, 2])).toBe(true);
+  expect(sameStructureAs([1, [1, 1]], [2, [2, 2]])).toBe(true);
+  expect(sameStructureAs([[[], []]], [[[], []]])).toBe(true);
+});
+
+it('returns false', () => {
+  expect(sameStructureAs([1, [1, 1]], [[2, 2], 2])).toBe(false);
+  expect(
+    sameStructureAs(
+      [1, [1, 1]],
+      [
+        [1, 1],
+        [1, 1],
+      ]
+    )
+  ).toBe(false);
+  expect(sameStructureAs([[2, 2], 2], [1, [1, 1]])).toBe(false);
+  expect(sameStructureAs([1, [1, 1]], [[2], 2])).toBe(false);
+  expect(sameStructureAs([[[], []]], [[1, 1]])).toBe(false);
+});
