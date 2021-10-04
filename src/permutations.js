831d96ad91ff5bc3455753808af0ce9f41ca7ebf
hextion <100ishundred@gmail.com>
2021-10-04 19:55:47 +0300

    Added permutations

---
+++ src/permutations.js
@@ -0,0 +1,28 @@
+/**
+ * In this kata you have to create all permutations of an input string and remove duplicates, if present.
+ * This means, you have to shuffle all letters from the input in all possible orders.
+ *
+ * @param {string} origin string
+ * @returns {string[]} all permutations of an input string without duplicates
+ */
+function permutations(origin) {
+  const entries = [{ prefix: '', chars: origin }];
+  const strings = new Set();
+
+  while (entries.length > 0) {
+    const { prefix, chars } = entries.pop();
+
+    for (let i = 0; i < chars.length; i++) {
+      const str = prefix.concat(chars[i]);
+      if (str.length === origin.length) strings.add(str);
+      else {
+        const nextChars = chars.slice(0, i).concat(chars.slice(i + 1));
+        entries.push({ prefix: str, chars: nextChars });
+      }
+    }
+  }
+
+  return Array.from(strings);
+}
+
+module.exports = { permutations };
