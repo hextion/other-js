7c64f31dfa15cb3a795536d6002f7adad6c59ba4
hextion <100ishundred@gmail.com>
2021-10-05 19:41:54 +0300

    Fixed lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,15 +1,7 @@
 const prettier = 'prettier --list-different';
 const eslint = 'eslint --max-warnings 0';
 
-/**
- * Return array of passed commands
- *
- * @param  {...string} commands commands to run
- * @returns {string[]} commands
- */
-const run = (...commands) => commands;
-
 module.exports = {
-  '*.{json,md,yml}': run(prettier),
-  '*.js': run(prettier, eslint),
+  '*.{json,md,yml}': [prettier],
+  '*.js': [prettier, eslint],
 };
