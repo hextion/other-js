76abd8aae9d129454cc5365ccae8b2fe0290ec94
hextion <100ishundred@gmail.com>
2021-10-05 20:46:15 +0300

    Replaced husky by simple-git-hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 const prettier = 'prettier --check';
-const eslint = 'eslint --max-warnings 0';
+const eslint = 'eslint --no-ignore --max-warnings 0';
 
 module.exports = {
   '*.{json,md,yml}': [prettier],
