e33f10d4c02e4646c1e1deea8a15dda08df7db0f
hextion <100ishundred@gmail.com>
2021-10-18 22:09:22 +0300

    Eslint config via js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:jsdoc/recommended'],
+  rules: {
+    'no-var': 'error',
+  },
+  root: true,
+};
