12988190666465e62a53aa36219bf1355f6c9a9e
hextion <100ishundred@gmail.com>
2021-10-24 16:33:46 +0300

    Refactor equalsAt

--- src/equal-sides-of-an-array.js
+++ src/equal-sides-of-an-array.js
@@ -5,19 +5,20 @@
  * @returns {number} index
  */
 function equalsAt(numbers) {
-  const leftToRight = numbers.reduce((acc, num) => {
-    const previous = acc[acc.length - 1] || 0;
-    return acc.concat(num + previous);
-  }, []);
-  const rightToLeft = numbers.reduceRight((acc, num) => {
-    const [previous = 0] = acc;
-    return [num + previous].concat(acc);
-  }, []);
-  for (let i = 0; i < numbers.length; i++) {
-    if (leftToRight[i] === rightToLeft[i]) {
-      return i;
+  let head = 0;
+  let tail = numbers.length - 1;
+  let headToTail = 0;
+  let tailToHead = 0;
+
+  while (head <= tail) {
+    if (tailToHead < headToTail) tailToHead += numbers[tail--];
+    if (headToTail < tailToHead) headToTail += numbers[head++];
+    if (headToTail === tailToHead) {
+      if (head === tail) return head;
+      else headToTail += numbers[head++];
     }
   }
+
   return 0;
 }
 
