13edbb6690263be27f1229f503864b274444b55d
hextion <100ishundred@gmail.com>
2021-10-24 16:36:37 +0300

    Increased equalsAt test coverage

--- src/__tests__/equal-sides-of-an-array.spec.js
+++ src/__tests__/equal-sides-of-an-array.spec.js
@@ -4,5 +4,6 @@ it('works', () => {
   expect(equalsAt([1, 2, 3, 4, 3, 2, 1])).toEqual(3);
   expect(equalsAt([1, 5, 4, 3, 2, 1])).toEqual(2);
   expect(equalsAt([6, 4, 3, 2, 1])).toEqual(1);
+  expect(equalsAt([1, 2, 3, 4, 6])).toEqual(3);
   expect(equalsAt([1, 4, 3, 2, 1])).toEqual(0);
 });
