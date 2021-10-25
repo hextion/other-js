01fb60283c6d3b181920ca7f4ecb88b4250641b7
hextion <100ishundred@gmail.com>
2021-10-25 19:10:50 +0300

    Improved equalsAt tests

--- src/__tests__/equal-sides-of-an-array.spec.js
+++ src/__tests__/equal-sides-of-an-array.spec.js
@@ -1,11 +1,14 @@
 const { equalsAt } = require('../equal-sides-of-an-array');
 
-it('works', () => {
-  expect(equalsAt([1, 2, 3, 4, 3, 2, 1])).toEqual(3);
-  expect(equalsAt([1, 5, 4, 3, 2, 1])).toEqual(2);
-  expect(equalsAt([6, 4, 3, 2, 1])).toEqual(1);
-  expect(equalsAt([1, 2, 3, 4, 6])).toEqual(3);
-  expect(equalsAt([10, 0, 0, 0, 0])).toEqual(0);
-  expect(equalsAt([0, 0, 0, 0, 3])).toEqual(4);
-  expect(equalsAt([1, 4, 3, 2, 1])).toEqual(-1);
+it.each`
+  array                    | index
+  ${[1, 2, 3, 4, 3, 2, 1]} | ${3}
+  ${[1, 5, 4, 3, 2, 1]}    | ${2}
+  ${[6, 4, 3, 2, 1]}       | ${1}
+  ${[1, 2, 3, 4, 6]}       | ${3}
+  ${[10, 0, 0, 0, 0]}      | ${0}
+  ${[0, 0, 0, 0, 3]}       | ${4}
+  ${[1, 4, 3, 2, 1]}       | ${-1}
+`('equal sides of the $array are at $index', ({ array, index }) => {
+  expect(equalsAt(array)).toBe(index);
 });
