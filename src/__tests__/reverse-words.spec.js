8124b959562ad900ca8d915a07949f14ef73c454
hextion <100ishundred@gmail.com>
2024-11-30 20:18:31 +0500

    reverse-words refactor

--- src/__tests__/reverse-words.spec.js
+++ src/__tests__/reverse-words.spec.js
@@ -1,8 +1,10 @@
 const { reverseWords } = require("../reverse-words");
 
 test("reverseWords", () => {
+  expect(reverseWords(" ")).toBe(" ");
   expect(reverseWords("i")).toBe("i");
   expect(reverseWords("i love you")).toBe("i evol uoy");
   expect(reverseWords("i'll be back")).toBe("ll'i eb kcab");
   expect(reverseWords("you're     awesome")).toBe("er'uoy     emosewa");
+  expect(reverseWords("     you're     awesome")).toBe("     er'uoy     emosewa");
 });
--- src/reverse-words.js
+++ src/reverse-words.js
@@ -1,8 +1,27 @@
-const { reverse: reverseString } = require("./reverse-string");
-const { reverse: reverseArray } = require("./reverse-array");
-
 function reverseWords(str) {
-  return reverseArray(reverseString(str).split(" ")).join(" ");
+  const result = [];
+  for (let i = 0, start = -1; i < str.length; i++) {
+    const char = str.at(i);
+    const isLast = i === str.length - 1;
+    const isSpace = char === " ";
+    if (isSpace || isLast) {
+      if (start === -1) {
+        result.push(char);
+      } else {
+        for (let j = isLast ? i : i - 1; j >= start; j--) {
+          result.push(str.at(j));
+        }
+        if (isSpace) {
+          result.push(char);
+        }
+        start = -1;
+      }
+    } else if (start === -1 && !isSpace) {
+      start = i;
+    }
+  }
+
+  return result.join("");
 }
 
 module.exports = { reverseWords };
